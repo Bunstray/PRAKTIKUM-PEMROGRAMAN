053143596ca90f4b44edf1367ce47bfc8dfe8ead
Bunstray <rayyanbunasatria06@gmail.com>
2024-09-13 15:43:05 +0700

    Add files via upload

--- Pertemuan 3/Soal 2/soal2.cpp
+++ Pertemuan 3/Soal 2/soal2.cpp
@@ -10,19 +10,17 @@ signed main(){
 	cout<<"\n3. ";
 	cin>>A[2];
 	cout<<"\n \n";
-	
 	//Metode brute force menggunakan percabangan
-	
-	if(A[0]>A[1]){
-		if(A[0]>A[2]){
+	if(A[0]>=A[1]){
+		if(A[0]>=A[2]){
 			cout<<"The biggest number is "<<A[0];
 		}
 		else{
 			cout<<"The biggest number is "<<A[2];
 		}
 	}
-	else if(A[1]>A[0]){
-		if(A[1]>A[2]){
+	else if(A[1]>=A[0]){
+		if(A[1]>=A[2]){
 			cout<<"The biggest number is "<<A[1];
 		}
 		else{
@@ -30,9 +28,7 @@ signed main(){
 		}
 	}
 	cout<<"\n";
-	
 	//lebih cepat menggunakan sort daripada percabangan
-	
 	//sort(A,A+3);
 	//cout<<"The biggest number is "<<A[2]<<"\n \n";
 	system("pause");
