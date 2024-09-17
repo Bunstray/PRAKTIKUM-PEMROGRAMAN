6c413079d254b189651ebc33715d8ddb24ed99f4
Bunstray <rayyanbunasatria06@gmail.com>
2024-09-17 14:11:34 +0700

    Add files via upload

---
+++ Pertemuan 4/soal2/Soal2.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+signed main(){
+	int n;
+	cout<<"Input : ";cin>>n;
+	cout<<"Output : \n";
+	for(int i=1;i<=10;i++){
+		cout<<n<<" x "<<i<<" = "<<n*i<<"\n";
+	}
+}
---
+++ Pertemuan 4/soal2/makefile
@@ -0,0 +1,8 @@
+soal2: soal2.o  
+	g++ -o soal2 soal2.o 
+
+soal2.o: soal2.cpp
+	g++ -c soal2.cpp
+
+clean:
+	rm -f soal2 soal2.o
