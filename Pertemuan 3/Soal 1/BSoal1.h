325586f0dd21afe07f01aaa9d60552937ac8fe78
Bunstray <rayyanbunasatria06@gmail.com>
2024-09-21 11:55:27 +0000

    gas

--- Pertemuan 3/Soal 1/BSoal1.h
+++
@@ -1,9 +0,0 @@
-#ifndef HELPER_H
-#define HELPER_H
-
-#include <string>
-#include <cmath>
-
-void formula(double A, double B, double C);
-
-#endif
---
+++ Pertemuan3/Soal1/soal1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cmath>
+#define ll long long
+using namespace std;
+
+int main(){
+	double A,B,C,d;
+	cout<<"<---Quadratic Formula--->"<<"\n"<<"\n";
+	cout<<"Ax^2 + Bx + C = 0"<<"\n";
+	cout<<"A = ";
+	cin>>A;
+	cout<<"B = ";
+	cin>>B;
+	cout<<"C = ";
+	cin>>C;
+	d=(B*B)-(4*A*C);
+	cout<<"--------------------------"<<"\n";
+	if(d>0){
+		double x1,x2;
+		x1=(((-B)+sqrt(d))/(2*A));
+		x2=(((-B)-sqrt(d))/(2*A));
+		cout<<"x1 = "<<x1<<"\n"<<"x2 = "<<x2<<"\n";
+	}
+	else if(d==0){
+		double x;
+		x=-B/(2*A);
+		cout<<"x = "<<x<<"\n";
+	}
+	else{
+		cout<<"There is no real roots (the roots are complex)"<<"\n";
+	}
+	
+}
