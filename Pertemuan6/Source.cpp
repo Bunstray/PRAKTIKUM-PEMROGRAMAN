65b438d30520b23deeefd2271a12d40fd4869350
Bunstray <rayyanbunasatria06@gmail.com>
2024-10-20 15:11:05 +0000

    q

---
+++ Pertemuan6/Source.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+struct necessity{
+	int code;
+	string name;
+	float price;
+	int amount;
+};
+
+signed main(){
+	int n;
+	float mean=0;
+	cout<<"How many kind(s) of stuff :";
+	cin>>n;
+	if(n>50){
+		cout<<"Error : Too much request";
+		return 0;
+	}
+	necessity stuff[n];
+	for(int i=0;i<n;i++){
+		cout<<"Item ("<<i+1<<")\n";
+		cout<<"Code : ";cin>>stuff[i].code;
+		cout<<"Name : ";cin>>stuff[i].name;
+		cout<<"Price : Rp";cin>>stuff[i].price;
+		cout<<"Amount : ";cin>>stuff[i].amount;
+		mean+=stuff[i].price;
+	}
+	mean/=n;
+	for(int i=0;i<n;i++){
+		cout<<"\nItem ("<<i+1<<")\n";
+		cout<<"Code : "<<stuff[i].code;
+		cout<<"\nName : "<<stuff[i].name;
+		cout<<"\nPrice : Rp"<<stuff[i].price;
+		cout<<"\nAmount : "<<stuff[i].amount<<"\n";
+	}
+	cout<<"\nAverage Price of"<<mean;
+}
